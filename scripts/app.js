2940821b4846cedc8a5cee0b02c14d6b18a0233e
Juan <juuanandre722@gmail.com>
2025-03-27 16:27:10 -0300

    style: unifies global styles and adjusts title spacing
    feat: starts site responsiveness
    
    feat: adds hamburger menu on mobile

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById('menu')
+const sideBar = document.querySelector('.sidebar')
+menu.addEventListener('click', () => {
+    sideBar.style.display = 'block'
+    
+})
+
+
+
