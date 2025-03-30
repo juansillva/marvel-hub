660c078e072a19a71f5e494c8dd27cebc6fb1fab
Juan <juuanandre722@gmail.com>
2025-03-30 13:27:17 -0300

    feat: finalize responsive adjustments, visual improvements
    - adds shadows to elements for better contrast and aesthetics.
    - adds button to close the sidebar for better user interaction.
    
    - updates movie and series names for accuracy and standardization.
    
    - modifies movie and series details with new information.
    
    - implements horizontal scroll for movies and series on mobile for smoother navigation.
    
    - adds footer with information about marvel hub, social media links, contact details, and copyright notice.
    
    - finalizes responsive adjustments, optimizing the layout for mobile devices.

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,23 @@
 const menu = document.getElementById('menu')
 const sideBar = document.querySelector('.sidebar')
 menu.addEventListener('click', () => {
-    sideBar.style.display = 'block'
-    
+    sideBar.style.transform = 'translateX(0)';
 })
 
+const closeSideBar = document.querySelector('.close-sidebar')
+
+closeSideBar.addEventListener('click', () => {
+    sideBar.style.transform = 'translateX(-100%)';
+})
+
+const postItem = document.querySelector('.post-item')
+const postItemBack = document.querySelector('.post-item-back')
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        duration: 800,  // Duração da animação (em ms)
+        easing: "ease-out",  // Tipo de transição
+        once: true  // Se true, a animação acontece só uma vez
+    });
+});
