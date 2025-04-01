7b5f458123a8b9f4b073098d8c50843963cdb23b
Juan <juuanandre722@gmail.com>
2025-04-01 00:37:37 -0300

    feat: implement initialize AOS animations

---
+++ scripts/libs.js
@@ -0,0 +1,8 @@
+//inicia AOS//
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        duration: 800, 
+        easing: "ease-out",  
+        once: true  
+    });
+});
